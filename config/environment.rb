525e523f283231d59f5b8680609b56ff76a43853
Josh <josh.pencheon@googlemail.com>
2008-11-18 11:30:25 +0000

    Remove jRails - prefer unobtrusive JS

--- config/environment.rb
+++ config/environment.rb
@@ -31,7 +31,6 @@ Rails::Initializer.run do |config|
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-  config.plugins = [ :jrails ]
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )

2861ad4eca09ed264d1d653248b945e3ed3c102e
Josh Pencheon <josh.pencheon@googlemail.com>
2008-11-25 13:02:08 +0000

    Added more CSS, leaves etc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   def update
     if @user.update_attributes(params[:user])
       flash[:notice] = "Successfully updated..."
-      redirect_to root_url
+      redirect_to @user
     else
       render :action => "edit"
     end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,7 @@
 jQuery(document).ready(function($) {
   $('a[rel*=facebox]').facebox({
 	  loadingImage : '/images/facebox/loading.gif',
-	  closeImage   : '/images/facebox/closelabel.gif'
+	  closeImage   : '/images/facebox/closelabel.gif',
+		opacity      : 0.3
 	}) 
 })
